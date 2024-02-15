85fff58a2b8b10d02eaf3abfb251c8c0cba8edd0
saedpanah <m.saedpanah76@gmail.com>
2024-02-15 11:30:52 +0330

    changes3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Person } from "@mui/icons-material";
 import { Button } from "@mui/material";
+
 
 function App() {
   return (
     <div>
-      <Button variant="contained"> سلام دنیا</Button>
+      <Button 
+      variant="contained"
+      size="large"
+      startIcon={<Person/>}
+      backgroundColor = "primary"
+      sx={{
+        color:'white'
+      }}
+      > 
+      سلام دنیا</Button>
     </div>
   );
 }
