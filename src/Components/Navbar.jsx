13c15a623383f578e345dbfc072ee3c6cc0f3d6a
saedpanah <m.saedpanah76@gmail.com>
2024-02-22 12:23:32 +0330

    changes7

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { alpha, AppBar, InputBase, Toolbar, Typography, useTheme, Badge } from '@mui/material'
-import { Search, Mail } from '@mui/icons-material'
+import { alpha, AppBar, InputBase, Toolbar, Typography, useTheme, Badge, Avatar, Container, Box } from '@mui/material'
+import { Search, Mail, Notifications } from '@mui/icons-material'
 
 
 const Navbar = () => {
@@ -35,36 +35,61 @@ const Navbar = () => {
               تاپ لرن
           </Typography>
 
-          <div 
-           style={{
+          <Container
+          sx={{
             display:"flex",
             alignItems:"center",
             backgroundColor:alpha(theme.palette.common.white,0.15),
             "&:hover":{
               backgroundColor:alpha(theme.palette.common.white,0.25),
             },
-            borderRadius:(theme.shape.borderRadius),
+            borderRadius:"4px",
             width:"40%",
+            [theme.breakpoints.down("sm")]:{
+              display:"none"
+            },
            }}
           >
-            <Search/>
+          <Search/>
             <InputBase placeholder="جستجو کنید"
               sx={{
                 color:"white",
                 marginRight:(theme.spacing(1.5))
               }}
             />
-          </div>
-          <div>
+          </Container>
+          <Box 
+          sx={{
+            display:"flex",
+            alignItems:"center"
+          }}
+          >
+          <Search
+           sx={{
+             marginLeft:(theme.spacing(2))
+           }}
+          />
           <Badge badgeContent={4} color="secondary"
           >
               <Mail color="action"
                 sx={{
-                  color:"white"
+                  color:"white",
+                  marginLeft:(theme.spacing(2))
                 }}
               />
           </Badge>
-          </div>
+          <Badge badgeContent={3} color="secondary"
+          >
+              <Notifications color="action"
+                sx={{
+                  color:"white",
+                  marginLeft:(theme.spacing(2))
+                }}
+              />
+          </Badge>
+
+          <Avatar alt="Cindy Baker" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwViW8fX3Gbj68tZ2XvH_PySnrpgiMsyBc6Gjq3ucmCPZoJjOQDmsVfv7B0A&s" />
+          </Box>
         </Toolbar>
       </AppBar>
      
