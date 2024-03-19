6b4fe4fa41e5bcf99befd0f6f0e9a3553514cb26
saedpanah <m.saedpanah76@gmail.com>
2024-03-19 10:00:49 +0330

    changes20

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -2,7 +2,7 @@ import {
    Fab, Modal, Tooltip,
    Container, useTheme, TextField,
    MenuItem, FormLabel ,RadioGroup ,
-   FormControlLabel,Radio
+   FormControl,Radio, Button, Select, FormControlLabel, InputLabel
   } 
 from '@mui/material'
 import { Add as AddIcon } from '@mui/icons-material'
@@ -15,13 +15,13 @@ const Add = () => {
     <div>
       <Tooltip title="افزودن پست" aria-label="add" onClick={()=>setOpen(true)}>
         <Fab color="primary"
-        sx={{
-            position:"fixed",
-            bottom:"10px",
-            left:"10px"
-        }}
-        >
-            <AddIcon/>
+          sx={{
+              position:"fixed",
+              bottom:"10px",
+              left:"10px"
+          }}
+          >
+          <AddIcon/>
         </Fab>
       </Tooltip>
       <Modal open={open}>
@@ -42,11 +42,15 @@ const Add = () => {
           }
          }}
         >
-         <form autoComplete='off'>
-         <div>
-         <TextField id="standard-basic" label="عنوان" variant="standard" style={{width:"100%"}}/>
+         <form autoComplete='off'
+         style={{
+          padding: theme.spacing(2)
+         }}
+         >
+         <div style={{marginBottom:theme.spacing(4)}}>
+         <TextField placeholder='عنوان' variant="standard" style={{width:"100%"}}/>
          </div>
-         <div>
+         <div style={{marginBottom:theme.spacing(4)}}>
          <TextField 
           id="outlined-multiline-static"
           label="پیام شما"
@@ -57,19 +61,25 @@ const Add = () => {
           style={{width:"100%"}}
           />
          </div>
-         <div>
-         <TextField select label="نوع دسترسی">
-            <MenuItem value="public">
-              عمومی
-            </MenuItem>
-            <MenuItem value="private">
-              خصوصی
-            </MenuItem>
-            <MenuItem value="friend">
-              نمایش برای دوستان
-            </MenuItem>
-
-          </TextField>
+         <div style={{marginBottom:theme.spacing(4)}}>
+         <FormControl sx={{width:"100%"}}>
+          <InputLabel htmlFor="filled-age-native-simple"
+          sx={{display:"block", width:"90%", transformOrigin:"top right"}}
+          >
+           انتخاب
+          </InputLabel>
+            <Select>
+              <MenuItem value="public">
+                عمومی
+              </MenuItem>
+              <MenuItem value="private">
+                خصوصی
+              </MenuItem>
+              <MenuItem value="friend">
+                نمایش برای دوستان
+              </MenuItem>
+            </Select>
+         </FormControl>
          </div>
          <div>
           <FormLabel id="demo-customized-radios">دسترسی کامنت گذاری</FormLabel>
@@ -100,6 +110,15 @@ const Add = () => {
               label="سفارشی (کاربران ویژه)"
             />
           </RadioGroup>
+         </div>
+         <div>
+          <Button variant='outlined' color="primary" sx={{marginLeft:"10px"}}>
+            ارسال
+          </Button>
+          <Button variant='outlined' color="secondary" onClick={()=>setOpen(false)}>
+            انصراف
+          </Button>
+
          </div>
          </form>
         </Container>
